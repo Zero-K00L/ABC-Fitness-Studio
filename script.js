bedb190d58cfd6fac9122a23ec7ad014fe93e559
John Grudzinski <johngrudzinski@yahoo.com>
2025-01-14 15:42:13 -0500

    Commit 9: Progress on cart functionality

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-// Mobile
+// Mobile Cart
+const cartIconMobile = document.querySelector('.cart-icon');
+const cartScreen = document.querySelector('.cart-inventory');
+
+// reveals and hides cart inventory screen when cart icon is pressed
+function revealCart() {
+   if (cartScreen.classList.contains('hidden')) {
+      cartScreen.classList.remove('hidden');
+      document.addEventListener('click', hideCart);
+   }
+   else {
+      cartScreen.classList.add('hidden');
+      document.removeEventListener('click', hideCart);
+   }
+}
+
+function hideCart(event) {
+   if(!cartScreen.contains(event.target) && event.target !=cartIconMobile 
+   && event.target !== cartIconDesktop && event.target !== cartBtn) {
+      cartScreen.classList.add('hidden');
+      document.removeEventListener('click', hideCart);
+   }
+}
+
+cartIconMobile.addEventListener('click', revealCart);
+cartIconDesktop.addEventListener('click', revealCart);
+
+// Mobile Hamburger Menu
 const btnHamburger = document.querySelector('#btnHamburger');
 const overlayAnim = document.querySelector('#overlay');
 const overlayAnim2 = document.querySelector('#overlay2');
 const fadeElems = document.querySelectorAll('.has-fade');
 const fadeElems2 = document.querySelectorAll('.has-fade2');
 const body = document.querySelector('body');
-
-const cartIcon = document.querySelector('.cart-icon');
-const cartScreen = document.querySelector('.cart-inventory');
-
 
 btnHamburger.addEventListener('click', function() {
    if(btnHamburger.classList.contains('header-open')) {    // close Hamburger menu 
